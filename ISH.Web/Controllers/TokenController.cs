4d00c25c480c4fda6a8da3a63887b87262842897
Filip Jovanov <filip.jovanov@students.finki.ukim.mk>
2023-09-10 02:47:03 +0200

    Add endpoint to check if token is valid

---
+++ ISH.Web/Controllers/TokenController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Integrated_Systems_Homework.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    [Authorize]
+    public class TokenController : ControllerBase
+    {
+        [HttpPost]
+        [Route("is-valid-token")]
+        public IActionResult TokenIsValid() => Ok();
+
+    }
+}
